c24e50c95e3dc91beed94f32c8287bd58711417b
Hasta <77826721+hastabudiawan@users.noreply.github.com>
2021-01-25 09:40:37 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const sidenav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sidenav);
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 600,
+  interval: 3000
+});
+
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+
+const materialbox = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialbox);
+
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll, {
+  scrollOffset: 50
+});  
+
+var mybutton = document.getElementById("topbtn");
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
